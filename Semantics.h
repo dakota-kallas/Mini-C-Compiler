d56a9144d013c6a023c2d9bbd131ee16666a7399
Dakota Kallas <kallas6990@uwlax.edu>
2022-12-12 17:33:42 -0600

    Added 2D array functionality

--- Semantics.h
+++ Semantics.h
@@ -38,15 +38,26 @@ struct Array
   int Size;
 };
 
+struct Array2D
+{
+  char *Name;
+  struct Array2D *Next;
+  int SizeX;
+  int SizeY;
+};
+
 /* Semantics Actions */
 extern struct ExprRes *doIntLit(char *digits);
 extern struct ExprRes *doBoolLit(char *boolean);
 extern struct ExprRes *doRval(char *name);
 extern struct ExprRes *doArrayRval(char *name, struct ExprRes *Res);
+extern struct ExprRes *do2DArrayRval(char *name, struct ExprRes *Res1, struct ExprRes *Res2);
 extern void createVariable(SymTab *curTable, char *name, char *type);
 extern struct InstrSeq *createArray(char *name, char *size);
+extern struct InstrSeq *create2DArray(char *name, char *xSize, char *ySize);
 extern struct InstrSeq *doAssign(char *name, struct ExprRes *Res1);
 extern struct InstrSeq *doArrayAssign(char *name, struct ExprRes *Res1, struct ExprRes *Res2);
+extern struct InstrSeq *do2DArrayAssign(char *name, struct ExprRes *Res1, struct ExprRes *Res2, struct ExprRes *Res3);
 extern struct ExprRes *doArithmetic(struct ExprRes *Res1, struct ExprRes *Res2, char *OpCode);
 extern struct ExprRes *doMod(struct ExprRes *Res1, struct ExprRes *Res2);
 extern struct ExprRes *doLogicalCompare(struct ExprRes *Res1, struct ExprRes *Res2, char *OpCode);
@@ -60,6 +71,7 @@ extern struct InstrSeq *doPrintString(char *String);
 extern struct InstrSeq *doPrintExpressionList(struct ExprResList *ExprList);
 extern struct InstrSeq *doRead(char *Id, struct InstrSeq *Next);
 extern struct InstrSeq *doReadArray(char *Id, struct ExprRes *Res, struct InstrSeq *Next);
+extern struct InstrSeq *doRead2DArray(char *Id, struct ExprRes *Res1, struct ExprRes *Res2, struct InstrSeq *Next);
 extern struct ExprRes *doEq(struct ExprRes *Res1, struct ExprRes *Res2);
 extern struct ExprRes *doEquality(struct ExprRes *Res1, struct ExprRes *Res2, char *OpCode);
 extern struct InstrSeq *doIf(struct ExprRes *bRes, struct InstrSeq *seq, struct InstrSeq *seqElse);
